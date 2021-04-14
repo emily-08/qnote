f08ef2c3127664988c0bc551e443a7bddaec14bf
emily-08 <applesuzuki08@iCloud.com>
2021-04-14 17:15:26 +0900

    最低限のメモ保存機能

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -2,5 +2,24 @@ class MemosController < ApplicationController
 before_action :authenticate_user!
 
   def index
+    @memos = Memo.all
+  end
+
+  def new
+    @memo = Memo.new
+  end
+
+  def create
+    @memo = Memo.new(memo_params)
+    if @memo.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def memo_params
+    params.require(:memo).permit(:title, :text).merge(user_id: current_user.id)
   end
 end
